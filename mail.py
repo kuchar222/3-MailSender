780900e158d236bb8627d5d14792fa3d201d5141
Marcin Kucharski <kucharskimarcin.mk@gmail.com>
2022-04-15 21:16:07 +0200

    z polskimi literami i context menagerem

--- mail.py
+++ mail.py
@@ -2,19 +2,32 @@
 """
 
 import smtplib
+import email
 
 class Email:
-    """służy do wysyałania wiadomości do wybranych osób na podstawie formularza
+    """służy do wysyałania wiadomości do wybranych osób na podstawie formularza (context menager)
     """
     def __init__(self, serwer_smtp, mail_username, mail_password):
         self.serwer = serwer_smtp
+        self.port = 587
         self.username = mail_username
         self.password = mail_password
+        self.smptObj = None
+
+    def __enter__(self):
+        self.smptObj = smtplib.SMTP(self.serwer, self.port)
+        self.smptObj.ehlo()  # tu jest sprawdzanie połaczenie powinna być odpowiedź: self.smptObj.ehlo()[0] == 250
+        self.smptObj.starttls()
+        self.smptObj.login(self.username, self.password)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.smptObj.quit()
 
     def _prepare_text(self, name, book_title):
-        return f'{name} przypominam Ci, że już najwyższa pora oddać mi {book_title}'.encode('utf-8')
+        return f'{name} przypominam Ci, że już najwyższa pora oddać mi \"{book_title}\"'
 
-    def send_reminding_mail(self, email, name, book_title):
+    def send_reminding_mail(self, email_, name, book_title):
         """wysyła otrzymaną treść wiadomości na wskazany adres poczty
 
         Args:
@@ -22,13 +35,8 @@ class Email:
             text (str): treść wiadomości.
         """
         text = self._prepare_text(name, book_title)
-        send_to = email
-        send_from = self.username
-        msg = f'From: {send_from} \nSubject: Pamietaj \n\n{text}'
-
-        smptObj = smtplib.SMTP(self.serwer, 587)
-        smptObj.ehlo()  # tu jest sprawdzanie połaczenie powinna być odpowiedź: smptObj.ehlo()[0] == 250
-        smptObj.starttls()
-        smptObj.login(self.username, self.password)
-        smptObj.sendmail(send_from, send_to, msg)
-        smptObj.quit()
+        send_to = email_
+        send_from = f'Marcin K. <{self.username}>'
+        msg = email.message_from_string(f'From: {send_from} \nSubject: Pamiętaj \n\n{text}')
+        msg.set_charset('utf-8')
+        self.smptObj.sendmail(send_from, send_to, msg.as_string())
--- main.py
+++ main.py
@@ -15,13 +15,10 @@ mail_password = getenv('MAIL_PASSWORD')
 shelve = Bookshelve(path)
 expired_books = shelve.find_book_to_return()
 if len(expired_books) > 0:
-    mail = Email(serwer_smtp, mail_username, mail_password)
     for book in expired_books:
         email, name, book_title = book
-        #try:
-        mail.send_reminding_mail(email, name, book_title)
+        with Email(serwer_smtp, mail_username, mail_password) as mail:
+            mail.send_reminding_mail(email, name, book_title)
         print(f'Wysłano przypomnienie do {name}')
-        # except:
-        #     print('Mamy problem z wysłaniem przypomnienia')
 else:
     print('Na chwilę obecną nie upłynął żaden termin oddania książek')
